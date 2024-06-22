11af752cd324ca69c24888fb206607c879060181
acnleditor2 <158589325+acnleditor2@users.noreply.github.com>
2024-06-22 18:53:33 -0500

    Add files via upload

--- main.go
+++ main.go
@@ -152,6 +152,7 @@ func HttpHandler(w http.ResponseWriter, req *http.Request) {
 		textFile.Close()
 	case "/voices":
 		voices := make([]string, len(config.Voices))
+
 		i := 0
 		for k := range config.Voices {
 			voices[i] = k
@@ -168,10 +169,14 @@ func HttpHandler(w http.ResponseWriter, req *http.Request) {
 }
 
 func main() {
+	if len(os.Args) != 1 && len(os.Args) != 2 {
+		os.Exit(1)
+	}
+
 	var configBytes []byte
 	var err error
 
-	if len(os.Args) < 2 || os.Args[1] == "-" {
+	if len(os.Args) == 1 || os.Args[1] == "-" {
 		configBytes, err = io.ReadAll(os.Stdin)
 	} else if strings.HasPrefix(os.Args[1], "http://") || strings.HasPrefix(os.Args[1], "https://") {
 		response, err := http.Get(os.Args[1])
